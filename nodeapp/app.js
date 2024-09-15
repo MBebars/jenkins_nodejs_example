58729fb6401b24f673944bf945146748a90edc30
Mohamed Bebars <102308974+MBebars@users.noreply.github.com>
2024-09-15 16:10:17 +0300

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from YAT DEPI! YAT 225 with Build Number TEST WEB HOOK')
+    res.send('Hello World from YAT DEPI! YAT 225 with Build Number TEST WEB HOOK and EC2')
   })
   
   app.listen(port, () => {
